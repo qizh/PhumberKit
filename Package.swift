adfe1d672d8c19cec3817bf6d07e7291966c6f28
Serhii <zh.send@gmail.com>
2020-07-14 20:56:05 +0700

    Another context setting

--- Package.swift
+++ Package.swift
@@ -6,10 +6,12 @@ import PackageDescription
 let package = Package(
     name: "PhumberKit",
 	platforms: [
-		SupportedPlatform.iOS(SupportedPlatform.IOSVersion.v12)
+//		SupportedPlatform.iOS(SupportedPlatform.IOSVersion.v13),
+		.iOS(.v13),
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+		/*
 		.library(
 			name: "PhumberKit",
 			targets: ["PhumberKit"]
@@ -19,10 +21,11 @@ let package = Package(
 			type: Product.Library.LibraryType.dynamic,
 			targets: ["PhumberKit"]
 		),
+		*/
 		.library(
-			name: "PhumberKitStatic",
+			name: "PhumberKistatic",
 			type: Product.Library.LibraryType.static,
-			targets: ["PhumberKit"]
+			targets: ["PhumberKitarget"]
 		),
     ],
     dependencies: [
@@ -32,16 +35,29 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "PhumberKitarget",
+			dependencies: [],
+			path: "Sources/PhumberKit/",
+			exclude: [],
+			resources: [
+				Resource.copy("Resources/PhoneNumberMetadata.json"),
+			]
+		),
+/*
         .target(
             name: "PhumberKit",
             dependencies: [],
 			resources: [
-				Resource.process("Resources/PhoneNumberMetadata.json"),
+//				Resource.process("Resources/PhoneNumberMetadata.json"),
+				Resource.copy("Resources/PhoneNumberMetadata.json"),
 			]
 		),
+*/
         .testTarget(
-            name: "PhumberKitTests",
-            dependencies: ["PhumberKit"]
+            name: "PhumberKitests",
+            dependencies: ["PhumberKitarget"],
+			path: "Tests/PhumberKitTests/"
 		)
     ]
 )
